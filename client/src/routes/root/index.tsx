38e1e9e61a8f48d413d5a55e0928ff56e424d88f
Ken Vega <ken.vega.g@gmail.com>
2026-08-07 18:29:43 -0500

    remove footer

--- client/src/routes/root/index.tsx
+++ client/src/routes/root/index.tsx
@@ -1,26 +1,16 @@
 import { Outlet, ScrollRestoration } from "react-router";
 
-import { Container, Separator } from "@/components/ui";
-
 import HeaderMain from "./components/header-main";
 
 export default function Root() {
   return (
-    <div className="grid min-h-screen grid-rows-[auto_1fr_auto] bg-background">
+    <div className="grid min-h-screen grid-rows-[auto_1fr] bg-background">
       <header className="sticky top-0 z-50 border-b border-border bg-background">
         <HeaderMain />
       </header>
       <main className="flex flex-col [&>*]:flex [&>*]:flex-1 [&>*]:flex-col">
         <Outlet />
       </main>
-      <footer className="border-t border-border">
-        <Container>
-          <Separator orientation="horizontal" decorative={true} />
-          <small className="block py-6 text-center text-sm text-muted-foreground">
-            Todos los derechos reservados © Kudos
-          </small>
-        </Container>
-      </footer>
       <ScrollRestoration />
     </div>
   );
